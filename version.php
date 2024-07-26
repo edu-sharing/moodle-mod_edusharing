f14f4914bdc3de7827de5aeb07b83a35892dd295
ziegler <ziegler@edu-sharing.net>
2024-07-26 08:02:39 +0200

    chore: bump versions

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component    = 'mod_edusharing';
-$plugin->version      = 2024071700;
+$plugin->version      = 2024072600;
 $plugin->requires     = 2022112809;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
-$plugin->release      = '8.1.4';
+$plugin->release      = '8.1.5';
