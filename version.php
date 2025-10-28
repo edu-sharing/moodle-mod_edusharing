4933dddcede5b9d370b551a6d9ee01404860ee03
Luca Bösch <luca.boesch@bfh.ch>
2025-10-28 12:19:48 +0100

    Set version to the number of moodle.org/plugins, resolves #19.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component    = 'mod_edusharing';
-$plugin->version      = 2025092507;
+$plugin->version      = 2025100800;
 $plugin->requires     = 2022112809;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
