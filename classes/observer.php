dd1742cf0d87ad21c3681c18b0b23a8ca9de2324
ziegler <ziegler@edu-sharing.net>
2026-02-23 12:09:30 +0100

    fix: course restoration

--- classes/observer.php
+++ classes/observer.php
@@ -280,7 +280,7 @@ class mod_edusharing_observer {
         $courseid  = $eventdata['courseid'];
         try {
             $helper = new RestoreHelper(new EduSharingService());
-            $helper->convert_inline_options(courseid: $courseid, userid: $userid);
+            $helper->convert_inline_options(courseid: (int)$courseid, userid: (int)$userid);
         } catch (Exception $exception) {
             debugging($exception->getMessage());
         }
