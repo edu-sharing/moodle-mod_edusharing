3295349d4623a65e59ec8622c90558cb16fb3d0f
ziegler <ziegler@edu-sharing.net>
2026-07-16 12:11:52 +0200

    fix: code style

--- classes/MoodleAboutApiCacheHandler.php
+++ classes/MoodleAboutApiCacheHandler.php
@@ -1,4 +1,20 @@
-<?php declare(strict_types=1);
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+declare(strict_types=1);
 
 namespace mod_edusharing;
 
@@ -14,8 +30,7 @@ use EduSharingApiClient\EduSharingNodeHelper;
  * @copyright  metaVentis GmbH — http://metaventis.com
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class MoodleAboutApiCacheHandler implements AboutApiCacheHandler
-{
+class MoodleAboutApiCacheHandler implements AboutApiCacheHandler {
     /**
      * @var EduSharingNodeHelper
      */
@@ -30,6 +45,7 @@ class MoodleAboutApiCacheHandler implements AboutApiCacheHandler
         $this->nodehelper = $nodehelper;
     }
 
+    // phpcs:disable -- Function cannot be lowercase as it implements an interface.
     /**
      * Returns the repository _about response, cached at application level.
      *
@@ -41,6 +57,7 @@ class MoodleAboutApiCacheHandler implements AboutApiCacheHandler
      * @throws coding_exception
      */
     public function getAboutApiCache(): array {
+        // phpcs:enable
         $cache = \cache::make('mod_edusharing', 'about');
         $about = $cache->get('about');
         if ($about === false) {
