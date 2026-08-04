bb83095ac4783abf4ada837433181e2b84678f5a
ziegler <ziegler@edu-sharing.net>
2026-08-04 09:35:22 +0200

    chore: bump test version

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->version      = 2026073000;
 $plugin->requires     = 2024100700;
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->cron         = 0;
-$plugin->release      = '7.7.8';
+$plugin->release      = '7.7.9';
