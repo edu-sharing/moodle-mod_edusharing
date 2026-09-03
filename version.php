0dcc1256f60eb28e2e7a2aa0e9f0b6e76610ccfc
ziegler <ziegler@edu-sharing.net>
2026-09-03 13:43:50 +0200

    fix: display settings for brockhaus and learningapps

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 $plugin->component    = 'mod_edusharing';
-$plugin->version      = 2026082800;
+$plugin->version      = 2026090300;
 $plugin->requires     = 2024100700;
 $plugin->maturity     = MATURITY_BETA;
 $plugin->cron         = 0;
